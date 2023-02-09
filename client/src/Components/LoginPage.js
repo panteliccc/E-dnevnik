530f6f48990555d38852451e3f92e4279fda6bdc
Ft-1989 <ficatejic@gmail.com>
2023-02-09 19:22:07 +0100

    a

--- client/src/Components/LoginPage.js
+++ client/src/Components/LoginPage.js
@@ -3,12 +3,18 @@ import React from 'react';
 export default function LoginPage(){
     return(
         <div className='login'>
-            <button className='btt--profesor'>Profesor</button>
-            <form action="">
-                <input type="email" name="email" id="" />
-                <input type="password" name="password" id="" />
-                <input type="submit" value="Prijavi se" />
-            </form>
+            <div className='login--container'>
+                <div className='login--switch'>
+                    <button className='btt--profesor'>Profesor</button>
+                    <button className='btt--ucenik'>Ucenik</button>
+                </div>           
+                <form className='form--login' action="">
+                    <p>Profesor</p>
+                    <input type="email" name="email" id="email" />
+                    <input type="password" name="password" id="password" />
+                    <input type="submit" value="Prijavi se" />
+                </form>
+            </div>        
         </div>
     )
 }
