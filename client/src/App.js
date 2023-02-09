5466727e0e03bbd206c762bb578ceb4cde9d656e
Ft-1989 <ficatejic@gmail.com>
2023-02-09 17:42:20 +0100

    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
+import React from 'react';
 import './style.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
     </div>
   );
 }
-
-export default App;
