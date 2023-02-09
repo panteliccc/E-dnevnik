7fd23fccc2ef5d6ebb07e153f807d064bad3f149
Ft-1989 <ficatejic@gmail.com>
2023-02-09 19:54:33 +0100

    a

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,24 @@ import LoginPage from './Components/LoginPage';
 import './style.css';
 
 export default function App() {
+
+  const [loginType, setLoginType] = React.useState("Profesor")
+  const switchBtts = document.getElementsByClassName("switchBtts")
+
+  function Login(event){
+    if(event.target.className === "btt--profesor switchBtts") setLoginType(() => "Profesor")
+    else setLoginType(() => "Ucenik")
+
+    for(var i = 0; i < switchBtts.length; i++) switchBtts[i].style.borderWidth = "1px"
+    event.target.style.borderWidth = "3px"
+  }
+
   return (
     <div className="App">
-      <LoginPage />
+      <LoginPage 
+        Login={Login}
+        loginType={loginType}
+      />
     </div>
   )
 }
--- client/src/Components/LoginPage.js
+++ client/src/Components/LoginPage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-export default function LoginPage(){
+export default function LoginPage(props){
     return(
         <div className='login'>
             <div className='login--container'>
                 <div className='login--switch'>
-                    <button className='btt--profesor'>Profesor</button>
-                    <button className='btt--ucenik'>Ucenik</button>
+                    <button className='btt--profesor switchBtts' onClick={props.Login}>Profesor</button>
+                    <button className='btt--ucenik switchBtts' onClick={props.Login}>Ucenik</button>
                 </div>           
                 <form className='form--login' action="">
-                    <p>Profesor</p>
+                    <p>{props.loginType}</p>
                     <input type="email" name="email" id="email" />
                     <input type="password" name="password" id="password" />
                     <input type="submit" value="Prijavi se" />
